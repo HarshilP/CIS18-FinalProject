a5bb6f5849f64e2971ec66ad30d5cad24173df3c
HarshilP <harshilpatel1625@gmail.com>
2019-07-25 23:55:33 -0700

    Add files via upload

---
+++ ProfileDesc.java
@@ -0,0 +1,11 @@
+//Class that saves all the user inforamtion entered
+//Creates new instance of object for every new user
+
+public class ProfileDesc {
+	public static ProfileDesc user1 = new ProfileDesc();
+	String username;
+	String[] hoursArray = new String[24];
+	String gender;
+	String age;
+	String description;
+}
